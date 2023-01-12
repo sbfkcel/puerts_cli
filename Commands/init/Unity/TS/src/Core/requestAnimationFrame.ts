7dc8d9ba5fbc5f8053db02d18d861a77f91b7861
fan <fan@fandeMacBook-Pro.local>
2023-01-12 19:06:39 +0800

    处理 requestAnimationFrame 模版运行时兼容问题

--- Commands/init/Unity/TS/src/Core/requestAnimationFrame.ts
+++ Commands/init/Unity/TS/src/Core/requestAnimationFrame.ts
@@ -2,17 +2,24 @@ interface AnimationFrame {
     requestAnimationFrame?:Function,
     cancelAnimationFrame?:Function,
     setTimeout?:Function,
-    clearTimeout?:Function
+    clearTimeout?:Function,
+    performance?:any
 };
 
 const obj:AnimationFrame = globalThis || window || {};
 const _requestAnimationFrame = obj.requestAnimationFrame || obj.setTimeout;
 const _cancelAnimationFrame =  obj.cancelAnimationFrame || obj.clearTimeout;
+const startTime = +new Date;
 
 let lastTime:number;
 const customRequestAnimationFrame = (callback:Function,intervalTime:number):number=>{
     const fun = ()=>{
-        const currentTime:number = performance.now();                                               // 当前时间
+        const currentTime:number = (()=>{                                                           // 当前时间（这里为了V8和Node运行时的兼容，performance 精度更高）
+            if(obj.performance){
+                return obj.performance.now();
+            };
+            return +new Date - startTime;
+        })();
         lastTime = lastTime || currentTime;                                                         // 防止首次执行无历史时间
         const timeCouse:number = currentTime - lastTime;                                            // 距离上一帧时间
         const isNext:boolean = intervalTime-timeCouse <= 0 || timeCouse === 0;                      // 判断是否执行下一帧
